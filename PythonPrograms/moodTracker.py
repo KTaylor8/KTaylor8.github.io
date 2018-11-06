29c50dd945809903b770f4b1a293a6fe92521df5
Katie Taylor <katiemxtaylor@gmail.com>
2018-11-06 15:30:06 -0800

    added variable definitions to function headers and edited variable names

--- PythonPrograms/moodTracker.py
+++ PythonPrograms/moodTracker.py
@@ -1,12 +1,21 @@
 import nltk  # natural language toolkit
 import json
+# sentiment is the connotation of a word (positive or negative)
 
 
-def classifyResp(resp):
+def classifyResp(userResp):
     """
     Function prepares sentiment classifier based on examples (to determine if text is overall positive or negative in meaning), prints overall sentiment of response, and returns {resp, mood}
-    >>> classifyResp("I love my best friend")
-    {"I love my best friend" : positive}
+
+    userResp = user's response about their day
+    posResps = list of (text string, "positive")
+    negResps = list of (text string, "negative")
+    filteredWords = list of words longer than 2 characters (filtered words)
+    resps = list of (list of polar words in a string, sentiment)
+    sortedWords = list of filtered words ordered by decreasing frequency
+
+    >>> classifyResp("My best friend is amazing")
+    {"My best friend is amazing" : positive}
     """
 
     pos = "positive"
@@ -28,16 +37,18 @@ def classifyResp(resp):
     resps = []
     filteredWords = []
 
-    # match possibly polar words to sentiment
-    for (words, sentiment) in posResps + negResps:
-        for el in words.split():
+    # match filtered (possibly polar) words to sentiment for each string
+    for (string, sentiment) in posResps + negResps:
+        for el in string.split():
             if len(el) >= 3:  # shorter words neutral
                 filteredWords.append(el.lower())
-        resps.append((filteredWords, sentiment))
-    wordFeatures = getWordFeatures(getRespsWords(resps))
+        resps.append(filteredWords, sentiment)
+        filteredWords.clear()
+    sortedWords = getWordFeatures(getRespsWords(resps))
 
     # compiles training (response, sentiment) pairs and prepares classifier
-    extractedFeatures = extractFeatures(wordFeatures, resp.split())
+    # ISSUE: apply_features should be returning a list-like obj but apparently it's a dict
+    extractedFeatures = extractFeatures(sortedWords, userResp.split())
     trainingData = nltk.classify.apply_features(extractedFeatures, resps)
     classifier = nltk.NaiveBayesClassifier.train(trainingData)
 
@@ -45,46 +56,62 @@ def classifyResp(resp):
     # but for now it just prints the overall sentiment
     mood = classifier.classify(extractedFeatures)
     print(f'Your response was overall {mood}')
-    moodData = {resp: mood}
+    moodData = {userResp: mood}
     return moodData
 
 
 def getRespsWords(resps):
     """
-    Function separates [(words filtered to be > 3 characters), sentiment] list of pairs into list of those words only
+    Function separates [(filtered words), sentiment] list of pairs into list of those words only
+
+    resps = list of (list of polar words in a string, sentiment)
+    sigWords = list of all filtered individual words from the string
+
     >>> getRespsWords([("love", "best", "friend"), "positive"])
-    ["love","best","friend"]
+    ["best","friend","amazing"]
     """
-    allWords = []
+    sigWords = []
     for (words, sentiment) in resps:
         # extend() adds each individual element, not whole list
-        allWords.extend(words)
-    return allWords
+        sigWords.extend(words)
+    return sigWords
 
 
-def getWordFeatures(wordlist):
+def getWordFeatures(sigWords):
     """
     Function reorders list of filtered words by decreasing frequency
+
+    sigWords = list of all filtered individual words from the string
+    sigWordsAndFreq = dictionary(?) of filtered words with their frequencies
+    sortedWords = list of filtered words wordered by decreasing frequency
     """
-    wordlist = nltk.FreqDist(wordlist)
-    wordFeatures = wordlist.keys()
-    return wordFeatures
+    sigWordsAndFreq = nltk.FreqDist(sigWords)
+    sortedWords = sigWordsAndFreq.keys()  # gets dict keys
+    return sortedWords
 
 
-def extractFeatures(featuresList, document):
+def extractFeatures(sortedWords, userResp):
     """
     Function is a feature extractor that compares words in response to words in list of possible words so unused words can be ignored and response can be tested against the training data
+
+    sortedWords = list of filtered words wordered by decreasing frequency
+    userResp = user's response about their day
+    features = dictionary of {"contains(polar word)": whether or not user input string contains polar word}
+
     """
-    documentWords = set(document)
+    userRespWords = set(userResp)
     features = {}
-    for word in featuresList:
-        features[f"contains{word}"] = (word in documentWords)
+    for word in sortedWords:
+        features[f"contains{word}"] = (word in userRespWords)
     return features
 
 
 def storeMood(moodData):
     """
     Function writes dictionaries of {resp: mood} to json file
+
+    moodData = (user input string, sentiment)
+
     """
     f = open("moodTrackerData.json", "a")
     f.write(f"\n{moodData}")
@@ -94,13 +121,17 @@ def storeMood(moodData):
 def main():
     """
     Program's main process: asks user about their day to determine their mood, with the option to repeat
+
+    repeat = whether the user wants to repeat the program or not
+    userResp = user's response about their day
+
     """
     repeat = ''
     while repeat != "yes":
-        userInput = input(
+        userResp = input(
             "Please describe your day and how you feel about it.\n"
         ).lower()
-        print(classifyResp(userInput))
+        print(classifyResp(userResp))
         # storeMood(classifyResp(userInput))
         # repeat = input("Do you want to make another entry?\n").lower()
 
